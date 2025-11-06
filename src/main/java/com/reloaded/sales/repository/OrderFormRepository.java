2de429e1c09f062caddab3179fa52912c5a49199
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2025-11-06 09:04:05 +0200

    Refactoring

--- src/main/java/com/reloaded/sales/repository/OrderFormRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reloaded.sales.repository;
-
-import com.reloaded.sales.model.OrderForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderFormRepository extends JpaRepository<OrderForm, Integer> {
-}
--- src/test/java/com/reloaded/sales/controller/PartnerControllerTest.java
+++
@@ -1,5 +0,0 @@
-package com.reloaded.sales.controller;
-
-public class PartnerControllerTest {
-
-}
