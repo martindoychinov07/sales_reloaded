96423c52ffdaacc6796695a53b925be9653f240e
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2025-12-25 10:49:48 +0200

    Adding the changes i made to the custom exceptions

---
+++ server/src/main/java/com/reloaded/sales/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.reloaded.sales.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+  public AlreadyExistsException(String message) {
+    super(message);
+  }
+}
