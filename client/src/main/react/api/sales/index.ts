9918d78536345c1fc107ea29ac1a8d5fbda7dcbe
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2026-01-07 09:53:44 +0200

    Adding the index file

---
+++ client/src/main/react/api/sales/index.ts
@@ -0,0 +1,71 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { AppUserDto } from './models/AppUserDto';
+export type { AuthUser } from './models/AuthUser';
+export type { ContactDto } from './models/ContactDto';
+export type { CsrfToken } from './models/CsrfToken';
+export type { EntryProductDto } from './models/EntryProductDto';
+export type { ExchangeDto } from './models/ExchangeDto';
+export type { JsonNode } from './models/JsonNode';
+export type { OrderEntryDto } from './models/OrderEntryDto';
+export { OrderFormDto } from './models/OrderFormDto';
+export { OrderFormView } from './models/OrderFormView';
+export { OrderTypeDto } from './models/OrderTypeDto';
+export type { PagedModelAppUserDto } from './models/PagedModelAppUserDto';
+export type { PagedModelContactDto } from './models/PagedModelContactDto';
+export type { PagedModelExchangeDto } from './models/PagedModelExchangeDto';
+export type { PagedModelOrderFormView } from './models/PagedModelOrderFormView';
+export type { PagedModelOrderTypeDto } from './models/PagedModelOrderTypeDto';
+export type { PagedModelProductDto } from './models/PagedModelProductDto';
+export type { PagedModelSettingDto } from './models/PagedModelSettingDto';
+export type { PagedModelTranslationDto } from './models/PagedModelTranslationDto';
+export type { PageMetadata } from './models/PageMetadata';
+export { Product } from './models/Product';
+export type { ProductDto } from './models/ProductDto';
+export { SettingDto } from './models/SettingDto';
+export type { TranslationDto } from './models/TranslationDto';
+
+export { $AppUserDto } from './schemas/$AppUserDto';
+export { $AuthUser } from './schemas/$AuthUser';
+export { $ContactDto } from './schemas/$ContactDto';
+export { $CsrfToken } from './schemas/$CsrfToken';
+export { $EntryProductDto } from './schemas/$EntryProductDto';
+export { $ExchangeDto } from './schemas/$ExchangeDto';
+export { $JsonNode } from './schemas/$JsonNode';
+export { $OrderEntryDto } from './schemas/$OrderEntryDto';
+export { $OrderFormDto } from './schemas/$OrderFormDto';
+export { $OrderFormView } from './schemas/$OrderFormView';
+export { $OrderTypeDto } from './schemas/$OrderTypeDto';
+export { $PagedModelAppUserDto } from './schemas/$PagedModelAppUserDto';
+export { $PagedModelContactDto } from './schemas/$PagedModelContactDto';
+export { $PagedModelExchangeDto } from './schemas/$PagedModelExchangeDto';
+export { $PagedModelOrderFormView } from './schemas/$PagedModelOrderFormView';
+export { $PagedModelOrderTypeDto } from './schemas/$PagedModelOrderTypeDto';
+export { $PagedModelProductDto } from './schemas/$PagedModelProductDto';
+export { $PagedModelSettingDto } from './schemas/$PagedModelSettingDto';
+export { $PagedModelTranslationDto } from './schemas/$PagedModelTranslationDto';
+export { $PageMetadata } from './schemas/$PageMetadata';
+export { $Product } from './schemas/$Product';
+export { $ProductDto } from './schemas/$ProductDto';
+export { $SettingDto } from './schemas/$SettingDto';
+export { $TranslationDto } from './schemas/$TranslationDto';
+
+export { AppUserService } from './services/AppUserService';
+export { AuthService } from './services/AuthService';
+export { ContactService } from './services/ContactService';
+export { ExchangeService } from './services/ExchangeService';
+export { OrderFormService } from './services/OrderFormService';
+export { OrderTypeService } from './services/OrderTypeService';
+export { PdfService } from './services/PdfService';
+export { ProductService } from './services/ProductService';
+export { ReportService } from './services/ReportService';
+export { RequestService } from './services/RequestService';
+export { SettingService } from './services/SettingService';
+export { TranslationService } from './services/TranslationService';
