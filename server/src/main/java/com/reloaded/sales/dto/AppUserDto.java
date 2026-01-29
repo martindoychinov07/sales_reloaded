baeb948a9f70fd21159a08f4cdf6ee70ff12985b
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2026-01-29 09:29:02 +0200

    Adding license for backend

--- server/src/main/java/com/reloaded/sales/dto/AppUserDto.java
+++ server/src/main/java/com/reloaded/sales/dto/AppUserDto.java
@@ -1,3 +1,9 @@
+/*
+ * /*
+ *  * Copyright 2026 Martin Doychinov
+ *  * Licensed under the Apache License, Version 2.0
+ *  */
+ */
 package com.reloaded.sales.dto;
 
 import jakarta.validation.constraints.Size;
